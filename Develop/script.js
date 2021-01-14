f9eff5ff2f3c2df42fd38e9c6c148e6c80a99851
Dom Rafiti <dom.rafiti@gmail.com>
2021-01-14 16:35:49 -0700

    version two which is a stable example of the code.

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ const generateBtn = document.querySelector("#generate");
 
 //Is it possible to store the variables in one object?
 const passwordCriteria = {
-  length: 0,
+  passLength: 0,
   lowerCase: "",
   upperCase: "",
   numbers: "",
@@ -27,74 +27,73 @@ function generatePassword() {
   alert("We'll be asking you questions to help generate your password");
 
   /*Inluding while loop for validation of length*/
-
-  console.log(passwordCriteria.length);
-
-  //stored as a string
-  while (!(passwordCriteria.length >= 8) || !(passwordCriteria.length <= 128)) {
-    passwordCriteria.length = prompt("How long does your password need to be (must be between 8-128 characters)");
+  //converted to integer
+  while (!(passwordCriteria.passLength >= 8) || !(passwordCriteria.passLength <= 128)) {
+    passwordCriteria.passLength = parseInt(
+      prompt("How long does your password need to be (must be between 8-128 characters)")
+    );
   }
 
   while (!passwordCriteria.lowerCase && !passwordCriteria.upperCase && !passwordCriteria.numbers && !passwordCriteria.specialChar) {
 
-    const password = "";
+    let password = " ";
 
     //Boolean operators
-    passwordCriteria.lowerCase = confirm("Does your password need lowercase characters (Select 'OK' for Yes and 'Cancel' for No)?");
+    passwordCriteria.lowerCase = confirm("Click OK to confirm including lower case characters.");
 
-    passwordCriteria.upperCase = confirm("Does your password need uppercase characters (Select 'OK' for Yes and 'Cancel' for No)?");
+    passwordCriteria.upperCase = confirm("Click OK to confirm including upper case characters.");
 
-    passwordCriteria.numbers = confirm("Does your password need numbers (Select 'OK' for Yes and 'Cancel' for No)?");
+    passwordCriteria.numbers = confirm("Click OK to confirm including numbers.");
 
-    passwordCriteria.specialChar = confirm("Does your password need special characters (Select 'OK' for Yes and 'Cancel' for No)?");
+    passwordCriteria.specialChar = confirm("Click OK to confirm including special characters.");
+
+
+    console.log(passwordCriteria.passLength);
+
+    console.log(passwordCriteria.lowerCase);
+
+    console.log(passwordCriteria.upperCase);
+
+    console.log(passwordCriteria.numbers);
+
+    console.log(passwordCriteria.specialChar);
+
+
+    /*including while loop for validation of selection and case correction*/
+    //pick guaranteed characters - picking those at the front of the password
+    //fischer yates algorithm
+
+    //if (criteria) guarateed characters to meet selected criteria
+
+    //separte statement for totally random
+
+    //put together.
+
+    console.log(password.length);
+
+    while (password.length < passwordCriteria.passLength) {
+      if (passwordCriteria.lowerCase == true) {
+        password += lowerSelect.charAt(Math.floor(Math.random() * lowerSelect.length));
+        console.log(password);
+      }
+
+      if (passwordCriteria.upperCase === true) {
+        password += upperSelect.charAt(Math.floor(Math.random() * upperSelect.length));
+        console.log(password);
+      }
+
+      if (passwordCriteria.numbers === true) {
+        password += numberSelect.charAt(Math.floor(Math.random() * numberSelect.length));
+        console.log(password);
+      }
+
+      if (passwordCriteria.specialChar === true) {
+        password += specialSelect.charAt(Math.floor(Math.random() * specialSelect.length));
+        console.log(password);
+      }
+    }
+    return password;
   }
-
-  console.log(passwordCriteria.length);
-
-  console.log(passwordCriteria.lowerCase);
-
-  console.log(passwordCriteria.upperCase);
-
-  console.log(passwordCriteria.numbers);
-
-  console.log(passwordCriteria.specialChar);
-
-
-  /*including while loop for validation of selection and case correction*/
-  //pick guaranteed characters - picking those at the front ofthe password
-  //fischer yates algorithm
-
-  //if (criteria) guarateed characters to meet selected criteria
-
-  //separte statement for totally random
-
-  //put together.
-
-
-
-  if (passwordCriteria.lowerCase == true) {
-    password += lowerSelect.charAt(Math.floor(Math.random() * lowerSelect.length));
-    console.log(password);
-  }
-
-  if (passwordCriteria.upperCase === true) {
-    password += upperSelect.charAt(Math.floor(Math.random() * upperSelect.length));
-    console.log(password);
-  }
-
-  if (passwordCriteria.numbers === true) {
-    password += upperSelect.charAt(Math.floor(Math.random() * upperSelect.length));
-    console.log(password);
-  }
-
-  if (passwordCriteria.specialChar === true) {
-    password += upperSelect.charAt(Math.floor(Math.random() * upperSelect.length));
-    console.log(password);
-  }
-
-  //password +=;
-
-  //return password;
 }
 
 // Write password to the #password input
@@ -103,10 +102,7 @@ function writePassword() {
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-
-generateBtn.addEventListener("click", writePassword);
-
+generateBtn.addEventListener("click", writePassword)
